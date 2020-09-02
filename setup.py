67f860a7a5d329ac1321c8f4cc808ea137f4a2e3
Arash Kia <arash.nkia@gmail.com>
2020-09-02 22:44:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.3'
+VERSION = '1.1'
 PACKAGE_NAME = 'wikitopic'
 AUTHOR = 'Arash Kia and Finbarr Murphy'
 AUTHOR_EMAIL = 'arash.nkia@gmail.com'
 URL = 'https://github.com/ConKruG/wikitopic'
 
-LICENSE = 'Apache License 2.0'
+LICENSE = 'GNU GPL v3.0'
 DESCRIPTION = 'Explicit online topic extraction for documents from Wikipedia'
 LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
