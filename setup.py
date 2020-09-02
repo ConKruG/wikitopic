4ebd7de364ff667201d82bc8c23d6a4356bec62a
Arash Kia <arash.nkia@gmail.com>
2020-09-02 23:09:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.1'
+VERSION = '1.1.1'
 PACKAGE_NAME = 'wikitopic'
 AUTHOR = 'Arash Kia and Finbarr Murphy'
 AUTHOR_EMAIL = 'arash.nkia@gmail.com'
